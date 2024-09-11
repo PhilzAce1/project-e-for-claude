f491ed33cf9bf090229890a0ef017eaf542ea8fa
Michael Tempest <mit@risika.com>
2024-09-11 13:18:56 +0100

    Update page.tsx

--- app/signin/[id]/page.tsx
+++ app/signin/[id]/page.tsx
@@ -29,8 +29,10 @@ export default async function SignIn({
   const authToken =
     cookies().get('sb-aryklmppwuliidmvzwpo-auth-token-code-verifier')?.value || null;
 
+    console.log('authToken', authToken)
   if(authToken) {
     console.log('authToken', authToken)
+    redirect(`/auth/callback?code=${authToken}`)
   }
 
   // Declare 'viewProp' and initialize with the default value

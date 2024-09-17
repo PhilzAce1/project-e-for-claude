364d90649b9733178423d3650e3955724ee97d6d
Michael Tempest <mit@risika.com>
2024-09-17 16:58:13 +0100

    Update PostHogProvider.tsx

--- components/PostHogProvider.tsx
+++ components/PostHogProvider.tsx
@@ -1,33 +1,29 @@
 'use client';
 
 import { PostHogProvider } from 'posthog-js/react'
-import { ReactNode } from 'react'
-
+import { ReactNode, useEffect } from 'react'
 import posthog from 'posthog-js'
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
-
-if (typeof window !== 'undefined') {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || 'default_key', {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://app.posthog.com',
-    loaded: (posthog) => {
-      if (process.env.NODE_ENV === 'development') posthog.debug()
-    }
-  })
-}
+import { usePathname } from 'next/navigation'
 
 export default function PHProvider({ children }: { children: ReactNode }) {
-  const router = useRouter()
+  const pathname = usePathname()
 
   useEffect(() => {
-    // Track page views
-    const handleRouteChange = () => posthog?.capture('$pageview')
-    router.events.on('routeChangeComplete', handleRouteChange)
-
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
+    if (typeof window !== 'undefined') {
+      posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || 'default_key', {
+        api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://app.posthog.com',
+        loaded: (posthog) => {
+          if (process.env.NODE_ENV === 'development') posthog.debug()
+        }
+      })
     }
   }, [])
+
+  useEffect(() => {
+    if (pathname) {
+      posthog.capture('$pageview')
+    }
+  }, [pathname])
 
   return <PostHogProvider client={posthog}>{children}</PostHogProvider>
 }

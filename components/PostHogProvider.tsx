58d17ab2f05ddad8e89fdabe5664c5c64cdb1dbf
Michael Tempest <mit@risika.com>
2024-09-17 16:50:16 +0100

    Update PostHogProvider.tsx

--- components/PostHogProvider.tsx
+++ components/PostHogProvider.tsx
@@ -1,12 +1,12 @@
-'use client'
-
 import { PostHogProvider } from 'posthog-js/react'
+import { ReactNode } from 'react'
+
 import posthog from 'posthog-js'
 import { useEffect } from 'react'
-import { useRouter } from 'next/navigation'
+import { useRouter } from 'next/router'
 
 if (typeof window !== 'undefined') {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || 'default_key', {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://app.posthog.com',
     loaded: (posthog) => {
       if (process.env.NODE_ENV === 'development') posthog.debug()
@@ -14,7 +14,7 @@ if (typeof window !== 'undefined') {
   })
 }
 
-export default function PHProvider({ children }) {
+export default function PHProvider({ children }: { children: ReactNode }) {
   const router = useRouter()
 
   useEffect(() => {

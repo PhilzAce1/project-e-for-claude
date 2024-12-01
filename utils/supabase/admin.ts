20e422bfb4c61adcaaf2d3df0e6c009d6511305c
Michael Tempest <info@michaeltempest.com>
2024-12-01 21:25:54 +0000

    Fixes typing issue

--- utils/supabase/admin.ts
+++ utils/supabase/admin.ts
@@ -1,7 +1,7 @@
 import { toDateTime } from '@/utils/helpers';
 import { stripe } from '@/utils/stripe/config';
 import { createClient } from '@supabase/supabase-js';
-import Stripe from 'stripe';
+import { Stripe } from 'stripe';
 import type { Database, Tables, TablesInsert } from 'types_db';
 
 type Product = Tables<'products'>;

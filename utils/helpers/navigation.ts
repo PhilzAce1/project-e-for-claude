a5c71576e33aba34d0305732644429f8b4132ace
Michael Tempest <info@michaeltempest.com>
2025-01-05 23:31:26 +0000

    Setup Content Orders page

--- utils/helpers/navigation.ts
+++ utils/helpers/navigation.ts
@@ -4,12 +4,14 @@ import {
     ArrowTrendingUpIcon,
     RocketLaunchIcon,
     BuildingOfficeIcon,
-    SparklesIcon
+    SparklesIcon,
+    DocumentTextIcon
   } from '@heroicons/react/24/outline'
   
   export const navigation = [
     { name: 'Dashboard', href: '/', icon: HomeIcon},
     { name: 'Opportunities', href: '/opportunities', icon: SparklesIcon},
+    { name: 'Content Orders', href: '/content-orders', icon: DocumentTextIcon},
     { name: 'Your Business', href: '/business-information', icon: BuildingOfficeIcon},
     { name: 'Keyword Rankings', href: '/rankings', icon: ArrowTrendingUpIcon},
     { name: 'Site Audit', href: '/site-audit', icon: ShieldExclamationIcon},

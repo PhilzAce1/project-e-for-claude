4f202919da1e7fc8ff17022f7b188b99489ddffd
Michael Tempest <info@michaeltempest.com>
2025-01-30 23:04:45 +0000

    Update CompetitorKeywordList.tsx

--- components/ui/CompetitorKeywordList.tsx
+++ components/ui/CompetitorKeywordList.tsx
@@ -143,7 +143,7 @@ const CompetitorKeywordList = ({ competitors, userId }: CompetitorKeywordListPro
                 <div className='flex items-center gap-4 mb-4'>
                     <Listbox value={selected} onChange={handleCompetitorChange}>
                     <Label className="block text-sm font-medium leading-6 text-gray-900">Show Competitor</Label>
-                    <div className="relative mt-2">
+                    <div className="relative">
                         <ListboxButton className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
                             <span className="block truncate">{selected?.domain}</span>
                             <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">

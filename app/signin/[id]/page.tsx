5d38cd7b02cb4fe35f2e780632f54f5cfc5ba3b4
Michael Tempest <info@michaeltempest.com>
2025-02-20 12:12:11 +0000

    Add new auth

--- app/signin/[id]/page.tsx
+++ app/signin/[id]/page.tsx
@@ -15,6 +15,7 @@ import ForgotPassword from '@/components/ui/AuthForms/ForgotPassword';
 import UpdatePassword from '@/components/ui/AuthForms/UpdatePassword';
 import SignUp from '@/components/ui/AuthForms/Signup';
 import ConfirmEmailContent from '@/components/ui/AuthForms/ConfirmEmail';
+import Image from 'next/image';
 
 export default async function SignIn({
   params,
@@ -37,7 +38,6 @@ export default async function SignIn({
   }
 
   const supabase = await createClient();
-
   const {
     data: { user }
   } = await supabase.auth.getUser();
@@ -49,67 +49,79 @@ export default async function SignIn({
   }
 
   return (
-    <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <Logo width="64" height="120" className="mx-auto w-auto" />
-        <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          {viewProp === 'forgot_password'
-            ? 'Reset Password'
-            : viewProp === 'update_password'
-            ? 'Update Password'
-            : viewProp === 'confirm_email'
-            ? 'Confirm Email'
-            : viewProp === 'signup'
-            ? 'Sign Up'
-            : 'Sign in to your account'}
-        </h2>
-      </div>
+    <div className="flex min-h-full flex-1 h-screen bg-white">
+      <div className="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+        <div className="mx-auto w-full max-w-sm lg:w-96">
+          <div>
+            <Logo width="64" height="64" className="h-20 w-auto" />
+            <h2 className="mt-8 text-2xl font-bold tracking-tight text-gray-900">
+              {viewProp === 'forgot_password'
+                ? 'Reset Password'
+                : viewProp === 'update_password'
+                ? 'Update Password'
+                : viewProp === 'confirm_email'
+                ? 'Confirm Email'
+                : viewProp === 'signup'
+                ? 'Create an account'
+                : 'Sign in to your account'}
+            </h2>
+            {viewProp === 'password_signin' && (
+              <p className="mt-2 text-sm text-gray-500">
+                Not a member?{' '}
+                <a href="/signin/signup" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                  Start your free trial
+                </a>
+              </p>
+            )}
+          </div>
 
-      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
-        <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
-          {viewProp === 'password_signin' && (
-            <PasswordSignIn
-              allowEmail={allowEmail}
-              redirectMethod={redirectMethod}
-            />
-          )}
-          {viewProp === 'email_signin' && (
-            <EmailSignIn
-              allowPassword={allowPassword}
-              redirectMethod={redirectMethod}
-              disableButton={searchParams.disable_button}
-            />
-          )}
-          {viewProp === 'forgot_password' && (
-            <ForgotPassword
-              allowEmail={allowEmail}
-              redirectMethod={redirectMethod}
-              disableButton={searchParams.disable_button}
-            />
-          )}
-          {viewProp === 'update_password' && (
-            <UpdatePassword redirectMethod={redirectMethod} />
-          )}
-          {viewProp === 'signup' && (
-            <SignUp allowEmail={allowEmail} redirectMethod={redirectMethod} />
-          )}
+          <div className="mt-10">
+            {viewProp === 'password_signin' && (
+              <PasswordSignIn
+                allowEmail={allowEmail}
+                redirectMethod={redirectMethod}
+              />
+            )}
+            {viewProp === 'email_signin' && (
+              <EmailSignIn
+                allowPassword={allowPassword}
+                redirectMethod={redirectMethod}
+                disableButton={searchParams.disable_button}
+              />
+            )}
+            {viewProp === 'forgot_password' && (
+              <ForgotPassword
+                allowEmail={allowEmail}
+                redirectMethod={redirectMethod}
+                disableButton={searchParams.disable_button}
+              />
+            )}
+            {viewProp === 'update_password' && (
+              <UpdatePassword redirectMethod={redirectMethod} />
+            )}
+            {viewProp === 'signup' && (
+              <SignUp allowEmail={allowEmail} redirectMethod={redirectMethod} />
+            )}
+            {viewProp === 'confirm_email' && (
+              <ConfirmEmailContent />
+            )}
 
-{viewProp !== 'update_password' && viewProp !== 'confirm_email'  && allowOauth && (
-            <OauthSignIn />
-          )}
-          {viewProp === 'confirm_email' && (
-            <ConfirmEmailContent />
-          )}
+            {viewProp !== 'update_password' && viewProp !== 'confirm_email' && allowOauth && (
+              <div className="mt-10">
+                <OauthSignIn />
+              </div>
+            )}
+          </div>
         </div>
-
-        {viewProp === 'password_signin' && (
-          <p className="mt-10 text-center text-sm text-gray-500">
-            Not a member?{' '}
-            <a href="/signin/signup" className="font-semibold leading-6 text-orange-600 hover:text-orange-500">
-              Sign up now!
-            </a>
-          </p>
-        )}
+      </div>
+      <div className="relative hidden w-0 flex-1 lg:block">
+        <img
+          src="/public/authbg.jpg"
+          alt="SEO Growth Over Time"
+          width={600}
+          height={800}
+          className="absolute inset-0 h-full w-full object-cover"
+        />
       </div>
     </div>
   );
